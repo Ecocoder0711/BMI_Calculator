01d915138c95752c2ca1a6b5ce29d3fda87ca167
Ecocoder0711 <sakshamkashyapgame80@gmail.com>
2026-01-29 22:07:40 +0530

    Add Windows runner files for high DPI support and console output
    - Created `runner.exe.manifest` to enable DPI awareness and support for Windows 10 and 11.
    - Implemented `utils.cpp` and `utils.h` for console creation and command line argument handling.
    - Developed `win32_window.cpp` and `win32_window.h` to manage a high DPI-aware Win32 window, including theme updates and message handling.

---
+++ lib/runapp.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:my_bmi/screen/myhomepage.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'BMI Calculator', home: const MyHomePage());
+  }
+}
